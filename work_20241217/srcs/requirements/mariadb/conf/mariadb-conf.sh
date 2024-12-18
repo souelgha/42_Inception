62554074cd4dedc67359f6be84657e7304e18a25
sonia42 <sonia42@incept.myguest.virtualbox.org>
2024-12-18 17:24:12 +0100

    20241218_ok

--- work_20241217/srcs/requirements/mariadb/conf/mariadb-conf.sh
+++ work_20241217/srcs/requirements/mariadb/conf/mariadb-conf.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
 #--------------mariadb start--------------#
-service mariadb start # start mariadb
-sleep 5 # wait for mariadb to start
+service mariadb start
+sleep 5 
 
 #--------------mariadb config--------------#
-# Create database if not exists
+
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
-
-# Create user if not exists
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# Grant privileges to user
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
-
-# Change root password 
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-
-# Flush privileges to apply changes
 mariadb -e "FLUSH PRIVILEGES;"
 
-#--------------mariadb restart--------------#
-# Shutdown mariadb to restart with new config
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+#--------------mariadb restart with new config--------------#
 
-# Restart mariadb with new config in the background to keep the container running
-# exec mysqld_safe 
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
 
