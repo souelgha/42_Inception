e8cad8e9049e6d5bf8b255b558462bed3f3d5d85
sonia42 <sonia42@incept.myguest.virtualbox.org>
2024-12-19 13:30:25 +0100

    final20241219

--- srcs/requirements/mariadb/conf/mariadb-conf.sh
+++ srcs/requirements/mariadb/conf/mariadb-conf.sh
@@ -11,7 +11,7 @@ mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYS
 mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
-#--------------restart de mariadb avec new config-------------#
+#--------------mariadb restart with new config--------------#
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# wp-cli installation
 sleep 15
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# wp-cli permission
+
 chmod +x wp-cli.phar
-# wp-cli dans bin
 mv wp-cli.phar /usr/local/bin/wp
+
 
 cd /var/www/html
 chmod -R 755 /var/www/html/
@@ -14,15 +13,13 @@ chown -R www-data:www-data /var/www/html
 
 wp core download --allow-root
 
-# creation du fichier config.php
 wp core config --dbhost=mariadb:3306 \
                 --dbname="$MYSQL_DB" \
                 --dbuser="$MYSQL_USER" \
                 --dbpass="$MYSQL_PASSWORD" \
                 --allow-root
-
 echo "ok. config"
-# installe wpress avec les details du domaine
+
 wp core install --url="$DOMAIN_NAME" \
                 --title="$WP_TITLE" \
                 --admin_user="$WP_ADMIN_N" \
@@ -30,16 +27,16 @@ wp core install --url="$DOMAIN_NAME" \
                 --admin_email="$WP_ADMIN_E" \
                 --allow-root
 echo "ok. install"
-#create a new user with the given username, email, password and role
+
 wp user create "$WP_U_NAME" "$WP_U_EMAIL" \
                 --user_pass="$WP_U_PASS" \
                 --allow-root
 echo "ok. create"
 
-# changement du port listen vers 9000
+# changement du port d ecoute
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
-# php-fpm
-mkdir -p /run/php
 
-# start php-fpm 
+# creer repertoire  php-fpm
+mkdir -p /run/php
+# demarre php-fpm service en premier plan pour outils de debug
 /usr/sbin/php-fpm7.4 -F
